665aa67d7b00e9a6516ca3ae60aaadc5e99a7e02
Hirano Daisuke <zzzz.xo@gmail.com>
2019-05-21 22:46:20 +0900

    PHP/Laravel 13-1

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,9 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
