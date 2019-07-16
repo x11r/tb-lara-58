2adadbe96dca81fbe179db380dd54f1775860df6
Hirano Daisuke <zzzz.xo@gmail.com>
2019-07-17 00:24:58 +0900

    PHP/Laravel 15

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -37,4 +37,16 @@ class NewsController extends Controller
 
         return redirect('admin/news/create');
     }
+
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            $posts = News::where('title', $cond_title)->get();
+        } else {
+            $posts = News::all();
+        }
+
+        return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
 }
